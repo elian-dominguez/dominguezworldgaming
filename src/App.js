c2fef2c90ff0a6a532e0b5ee2ed287974362006b
Elian Dominguez <56704805+elian-dominguez@users.noreply.github.com>
2022-03-08 18:20:34 -0300

    Creación de la carpeta components y navbar del e-commerce

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
 import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,40 @@
+// import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap'
+import './NavBar.css'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+const NavBar = () => {
+  return (
+      <Navbar collapseOnSelect expand="lg" className='colorNav' variant="dark">
+          <Container>
+              <Navbar.Brand href="#home">World Gaming</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="me-auto">
+                      <Nav.Link href="#features">PC</Nav.Link>
+                      <Nav.Link href="#pricing">PlayStation</Nav.Link>
+                      <Nav.Link href="#pricing">Xbox</Nav.Link>
+                      <Nav.Link href="#pricing">Nintendo</Nav.Link>
+                      <NavDropdown title="Juegos" id="collasible-nav-dropdown">
+                          <NavDropdown.Item href="#action/3.1">Acción</NavDropdown.Item>
+                          <NavDropdown.Item href="#action/3.2">Deportes</NavDropdown.Item>
+                          <NavDropdown.Item href="#action/3.3">Aventura</NavDropdown.Item>
+                          <NavDropdown.Divider />
+                          <NavDropdown.Item href="#action/3.4">Otros</NavDropdown.Item>
+                      </NavDropdown>
+                  </Nav>
+                  <Nav>
+                      <Nav.Link href="#deets">Carrito</Nav.Link>
+                      {/* <Nav.Link eventKey={2} href="#memes">
+                          Dank memes
+                      </Nav.Link> */}
+                  </Nav>
+              </Navbar.Collapse>
+          </Container>
+      </Navbar>
+  )
+}
+
+export default NavBar
